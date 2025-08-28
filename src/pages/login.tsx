395ab2d96cfc863917241cec04bd8ca9b7eaec14
NovaCat <sparkstorms2@gmail.com>
2025-08-29 07:58:23 +0900

    Feat: update Chakra UI

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,13 @@
+"use client"
+
+import { Box } from "@chakra-ui/react"
+
 export default function Login() {
   return (
     <section>
-      Login Page
+        <Box background="tomato" width="100%" padding="4" color="white">
+            Login Here!
+        </Box>
     </section>
   )
 }
