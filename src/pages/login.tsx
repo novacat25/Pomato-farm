fa5cfc099172979deb95649068102c425642953f
NovaCat <sparkstorms2@gmail.com>
2025-08-29 08:22:20 +0900

    Feat: WireFrame of Login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,12 +1,21 @@
-"use client"
-
-import { Box } from "@chakra-ui/react"
+import { Box, Button, Flex, Input, SkeletonCircle, Text } from "@chakra-ui/react"
 
 export default function Login() {
   return (
     <section>
-        <Box background="tomato" width="100%" padding="4" color="white">
-            Login Here!
+        <Box 
+            background="lightgrey" 
+            padding="4" 
+        >
+            <Button>서비스 소개</Button>
+            <SkeletonCircle justifySelf="center" size={{ mdTo2xl: 96, base: 48 }} />
+            <Text textAlign="center">당신의 생산성을 위한 토마토 농사</Text>
+            <Input />
+            <Input />
+            <Flex gap={4}>
+                <Button>Login</Button>
+                <Button>Sign up</Button>
+            </Flex>
         </Box>
     </section>
   )
