9e0e1bab49abba9548ffa5fd97f3218fdb411868
NovaCat <sparkstorms2@gmail.com>
2025-09-03 09:54:12 +0900

    [Draft] Main Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,20 @@
+import { Button, Text, Flex, Grid, GridItem } from "@chakra-ui/react"
+
 export default function Home() {
   return (
     <section>
-      Initialised App
+      <Flex id="user-information" justifyContent="flex-end" alignItems="center" gap={4}>
+        <Text>Pomato-farmer-1</Text>
+        <Button>Login</Button>
+      </Flex>
+      <Grid templateColumns="repeat(2, 1fr)" gap="6">
+        <GridItem>
+          A
+        </GridItem>
+        <GridItem>
+          B
+        </GridItem>
+      </Grid>
     </section>
   )
 }
