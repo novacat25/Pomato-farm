0b79f4912ddef65973ff9afbdc86006fc27d878f
NovaCat <sparkstorms2@gmail.com>
2025-09-08 20:35:54 +0900

    Feat: design skeleton

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,34 @@
-import { Button, Text, Flex, Grid, GridItem } from "@chakra-ui/react"
+import { Button, Text, Flex, Grid, GridItem, Box, SkeletonCircle, NumberInput } from "@chakra-ui/react"
 
 export default function Home() {
   return (
-    <section>
+    <Box padding={{ mdTo2xl: 8, base: 4 }}>
       <Flex id="user-information" justifyContent="flex-end" alignItems="center" gap={4}>
         <Text>Pomato-farmer-1</Text>
-        <Button>Login</Button>
+        <Button>LogOut</Button>
       </Flex>
-      <Grid templateColumns="repeat(2, 1fr)" gap="6">
+      <Grid justifyItems="center" templateColumns={{ mdTo2xl: "65% 35%", base: "auto" }} gap="6">
         <GridItem>
-          A
+          <Flex direction="column" gap={4}>
+            <SkeletonCircle justifySelf="center" size={{ mdTo2xl: 96, base: 48 }} />
+              <NumberInput.Root width="200px" defaultValue="15" min={15} max={45}>
+                <NumberInput.Control />
+                <NumberInput.Input />
+              </NumberInput.Root>
+              <Text>
+                오늘 파밍한 토마토 수
+              </Text>
+              <Text>
+                ToDo List
+              </Text>
+          </Flex>
         </GridItem>
         <GridItem>
-          B
+          <Box>
+            관리 툴
+          </Box>
         </GridItem>
       </Grid>
-    </section>
+    </Box>
   )
 }
