d828750d2448e56428a407afc5a7b055a878c518
NovaCat <sparkstorms2@gmail.com>
2025-09-15 20:57:48 +0900

    Feat: check if a user is loginned

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,6 +19,7 @@ export default function Home() {
       if (!user) {
         router.push("/login")
       } else {
+        console.log(user)
         setIsLoading(false)
         user.displayName ? setUserName(user.displayName) : setUserName(DEFAULT_DISPLAY_NAME)
       }
