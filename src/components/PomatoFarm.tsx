b45eb5de66cad040adbe2e5ed2f3272553f2a503
NovaCat <sparkstorms2@gmail.com>
2025-09-15 21:48:47 +0900

    Feat: prop test(passing user information)

--- src/components/PomatoFarm.tsx
+++ src/components/PomatoFarm.tsx
@@ -1,10 +1,32 @@
-import { Text, Flex, SkeletonCircle, NumberInput } from "@chakra-ui/react"
+import { Text, Flex, SkeletonCircle, NumberInput, Box } from "@chakra-ui/react"
+import { User } from "firebase/auth"
 
-export const PomatoFarm = () => {
+type Props = {
+    user: User | null | undefined
+}
+
+export const PomatoFarm = ({ user }: Props) => {
+  const onClick = () => {
+    console.log(user)
+  }
+
   return (
     <Flex direction="column" gap={4}>
-      <SkeletonCircle justifySelf="center" size={{ mdTo2xl: 96, base: 48 }} />
-        <NumberInput.Root width="200px" defaultValue="15" min={15} max={45}>
+      <Box
+        cursor="pointer"
+        onClick={onClick}
+      >
+        <SkeletonCircle 
+          justifySelf="center" 
+          size={{ mdTo2xl: 96, base: 48 }} 
+        />
+      </Box>
+        <NumberInput.Root 
+          width="200px" 
+          defaultValue="15" 
+          min={15} 
+          max={45}
+        >
           <NumberInput.Control />
           <NumberInput.Input />
         </NumberInput.Root>
