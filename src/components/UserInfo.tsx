e9535b06c7e17bbc20923d1c3b55c332b94adcff
NovaCat <sparkstorms2@gmail.com>
2025-11-17 14:09:16 +0900

    Feat: Changed UserInfo Component by Menu style

--- src/components/UserInfo.tsx
+++ src/components/UserInfo.tsx
@@ -3,9 +3,10 @@
 import React from 'react'
 import { auth } from "../utils/firebase"
 import { useRouter } from "next/navigation"
-import { Text, Flex, Button } from "@chakra-ui/react"
+import { Text, Flex, IconButton, Menu, Portal } from "@chakra-ui/react"
 import { User } from "firebase/auth"
 import { DEFAULT_DISPLAY_NAME, LOGOUT_CONFIRM_MESSAGE } from '@/constants'
+import { LuUser } from "react-icons/lu"
 
 type Props = {
   user: User | null | undefined
@@ -31,8 +32,30 @@ export const UserInfo = ({ user, isEditMode = false }: Props) => {
   return (
       <Flex id="user-information" justifyContent="flex-end" alignItems="center" gap={4}>
         <Text>{user?.displayName ?? DEFAULT_DISPLAY_NAME}</Text>
-        {!isEditMode && <Button onClick={()=>router.push("/edit-profile")}>Edit</Button>}
-        <Button onClick={onClickLogout}>LogOut</Button>
+        <Menu.Root>
+          <Menu.Trigger asChild>
+            <IconButton 
+              rounded="full"  
+              size="sm" 
+              aria-label="user-info"
+              variant="outline"
+              color="tomato" 
+            >
+              <LuUser />
+            </IconButton>
+          </Menu.Trigger>
+          <Portal>
+            <Menu.Positioner>
+              <Menu.Content>
+                {!isEditMode 
+                  ? <Menu.Item onClick={()=>router.push("/edit-profile")} value="edit-profile">회원 정보</Menu.Item>
+                  : <Menu.Item onClick={()=>router.push("/")} value="main-page">메인 화면</Menu.Item>
+              }
+                <Menu.Item onClick={onClickLogout} value="sign-out">로그아웃</Menu.Item>
+              </Menu.Content>
+            </Menu.Positioner>
+          </Portal>
+        </Menu.Root>
       </Flex>
   )
 }
