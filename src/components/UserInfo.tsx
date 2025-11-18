92bb746eee56e141ae26770c47e10f29a6e0eede
NovaCat <sparkstorms2@gmail.com>
2025-11-18 14:23:31 +0900

    Feat: logout dialog popup

--- src/components/UserInfo.tsx
+++ src/components/UserInfo.tsx
@@ -1,12 +1,22 @@
 "use client"
 
-import React from 'react'
+import React from "react"
 import { auth } from "../utils/firebase"
 import { useRouter } from "next/navigation"
-import { Text, Flex, IconButton, Menu, Portal } from "@chakra-ui/react"
+import {
+  Text,
+  Flex,
+  IconButton,
+  Menu,
+  Button,
+  CloseButton,
+  Dialog,
+  Portal,
+} from "@chakra-ui/react"
 import { User } from "firebase/auth"
-import { DEFAULT_DISPLAY_NAME, message } from '@/constants'
+import { DEFAULT_DISPLAY_NAME, message } from "@/constants"
 import { LuUser } from "react-icons/lu"
+import { colors } from "@/constants/palette"
 
 type Props = {
   user: User | null | undefined
@@ -17,50 +27,83 @@ export const UserInfo = ({ user, isEditMode = false }: Props) => {
   const router = useRouter()
 
   const onClickLogout = async () => {
-    const ok = confirm(message.info.LOGOUT_CONFIRM_MESSAGE)
-    
-    if(ok) {
-      try {
-        await auth.signOut()
-        router.push("/login")
-      } catch (err) {
-        console.error(err)
-      }
+    try {
+      await auth.signOut()
+      router.push("/login")
+    } catch (err) {
+      console.error(err)
     }
-  }  
+  }
 
   return (
-      <Flex 
-        id="user-information" 
-        justifyContent="flex-end" 
-        alignItems="center" 
+    <Dialog.Root>
+      <Flex
+        id="user-information"
+        justifyContent="flex-end"
+        alignItems="center"
         gap={4}
         marginBottom={4}
       >
         <Text>{user?.displayName ?? DEFAULT_DISPLAY_NAME}</Text>
         <Menu.Root>
           <Menu.Trigger asChild>
-            <IconButton 
-              rounded="full"  
-              size="sm" 
+            <IconButton
+              rounded="full"
+              size="sm"
               aria-label="user-info"
               variant="outline"
             >
               <LuUser />
             </IconButton>
           </Menu.Trigger>
-          <Portal>
-            <Menu.Positioner>
-              <Menu.Content>
-                {!isEditMode 
-                  ? <Menu.Item onClick={()=>router.push("/edit-profile")} value="edit-profile">회원 정보</Menu.Item>
-                  : <Menu.Item onClick={()=>router.push("/")} value="main-page">메인 화면</Menu.Item>
-              }
-                <Menu.Item onClick={onClickLogout} value="sign-out">로그아웃</Menu.Item>
-              </Menu.Content>
-            </Menu.Positioner>
-          </Portal>
+          <Menu.Positioner>
+            <Menu.Content>
+              {!isEditMode ? (
+                <Menu.Item
+                  onClick={() => router.push("/edit-profile")}
+                  value="edit-profile"
+                >
+                  회원 정보
+                </Menu.Item>
+              ) : (
+                <Menu.Item onClick={() => router.push("/")} value="main-page">
+                  메인 화면
+                </Menu.Item>
+              )}
+              <Dialog.Trigger asChild>
+                <Menu.Item value="sign-out">로그아웃</Menu.Item>
+              </Dialog.Trigger>
+            </Menu.Content>
+          </Menu.Positioner>
         </Menu.Root>
       </Flex>
+      <Portal>
+        <Dialog.Backdrop />
+        <Dialog.Positioner>
+          <Dialog.Content>
+            <Dialog.Header>
+              <Dialog.Title>로그아웃</Dialog.Title>
+            </Dialog.Header>
+            <Dialog.Body>{message.info.LOGOUT_CONFIRM_MESSAGE}</Dialog.Body>
+            <Dialog.Footer>
+              <Dialog.ActionTrigger asChild>
+                <Button
+                  backgroundColor={colors.primary.main}
+                  onClick={onClickLogout}
+                >
+                  네
+                </Button>
+              </Dialog.ActionTrigger>
+              <Dialog.ActionTrigger asChild>
+                <Button variant="outline">아니오</Button>
+              </Dialog.ActionTrigger>
+            </Dialog.Footer>
+            <Dialog.CloseTrigger asChild>
+              <CloseButton size="sm" />
+            </Dialog.CloseTrigger>
+          </Dialog.Content>
+        </Dialog.Positioner>
+      </Portal>
+    </Dialog.Root>
   )
 }
